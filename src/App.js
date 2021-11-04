79d2b4a9b7a7b04c4e17a932827c1864e1fc5e5f
Rafal Almakiewicz <ralmakiewicz@gmail.com>
2021-11-04 22:14:25 +0100

    Add basic routing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DecksList } from "./components/decksList";
+import { Switch, Route, Redirect } from "react-router";
+import { Learn } from "./components/learn";
+import fetchData from "./app/fetchData";
+import { useDispatch } from "react-redux";
 
 function App() {
-  return <DecksList />;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
+  return (
+    <Switch>
+      <Route path="/learn">
+        <Learn />
+      </Route>
+      <Route path="/decks">
+        <DecksList />
+      </Route>
+      <Redirect from="/" exact to="/decks" />
+    </Switch>
+  );
 }
 
 export default App;
